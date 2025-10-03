f29067edd775a0abd2128028446f311251b92294
Ali Aryani <lenesis@proton.me>
2025-10-03 21:35:21 +0330

    guessing game -> comment debug lines

--- 0002-guessing-game/guessing-game/src/main.rs
+++ 0002-guessing-game/guessing-game/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
 
     let secret_number = rand::thread_rng()
         .gen_range(1, 101);
-    println!("The secret_number is: {}",secret_number);
+    // println!("The secret_number is: {}",secret_number);
 
     loop {
         println!("Please input your guess.");
@@ -22,7 +22,7 @@ fn main() {
           let guess: u32 = guess.trim().parse()
           .expect("Please type a number!");
 
-        println!("You guessed: {}", guess);
+        // println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
