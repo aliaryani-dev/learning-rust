b30064601f8d3af0c61ed222bf2e0298471a1913
Ali Aryani <lenesis@proton.me>
2025-10-05 22:28:13 +0330

    const

--- 0003-variables/variables/src/main.rs
+++ 0003-variables/variables/src/main.rs
@@ -4,4 +4,7 @@ fn main() {
     // x = 6; // ERROR
     x = 6;
     println!("The value of x is {}", x);
+
+    const MAX_POINT:u32 = 100_000; // a constant here is like constexpr in C++
+    println!("MAX_POINT: {}", MAX_POINT);
 }
