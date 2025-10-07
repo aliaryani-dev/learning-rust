59441a0b5ccd2a592efed4af7f7400c31f3c7c47
Ali Aryani <lenesis@proton.me>
2025-10-07 23:54:54 +0330

    iteration with for

--- 0007-loops/loops/src/main.rs
+++ 0007-loops/loops/src/main.rs
@@ -3,10 +3,17 @@ fn main() {
     //loop {
     //    println!("again!");
     // }
+    // while loop
     let mut number = 5;
     while number != 0 {
         println!("again!");
         number -= 1;
     }
     println!("LIFTOFF!!!");
+    
+    // for loop for iteration
+    let a = [10,20,30,40,50];
+    for element in a.iter() {
+        println!("the value is {}", element);
+    }
 }
