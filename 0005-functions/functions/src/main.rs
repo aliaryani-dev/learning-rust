89b9cc465a452b2542cf0541581aab7b79f6d06d
Ali Aryani <lenesis@proton.me>
2025-10-07 22:55:19 +0330

    function parameter

--- 0005-functions/functions/src/main.rs
+++ 0005-functions/functions/src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
     another_function();
+    yet_another_function(58);
 }
 
 fn another_function() {
     println!("Another function");
+}
+
+fn yet_another_function(x:i32) {
+    println!("Result: {}", x+2);
 }
