41ce8dc14d01f7a4982eaa8204f6cce79e93546c
Ali Aryani <lenesis@proton.me>
2025-10-07 23:47:19 +0330

    while loop

--- 0007-loops/loops/src/main.rs
+++ 0007-loops/loops/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     // loop loop 
-    loop {
+    //loop {
+    //    println!("again!");
+    // }
+    let mut number = 5;
+    while number != 0 {
         println!("again!");
+        number -= 1;
     }
+    println!("LIFTOFF!!!");
 }
