a95db7fab6bf6b49846020cf5dd4825dbac1a490
Ali Aryani <lenesis@proton.me>
2025-10-07 23:05:13 +0330

    function with return value

--- 0005-functions/functions/src/main.rs
+++ 0005-functions/functions/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     another_function();
     yet_another_function(58);
+    let x = five();
+    println!("x: {}", x);
 }
 
 fn another_function() {
@@ -9,4 +11,8 @@ fn another_function() {
 
 fn yet_another_function(x:i32) {
     println!("Result: {}", x+2);
+}
+
+fn five() -> i32{
+    5 // as long as there is no semicolon at the end, this is an expression.
 }
