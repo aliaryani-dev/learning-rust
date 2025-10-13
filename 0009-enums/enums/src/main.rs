c0327cc2e8e2ec6b5cb1dc75d827719d55aec245
Ali Aryani <lenesis@proton.me>
2025-10-14 00:45:09 +0330

    use in function

--- 0009-enums/enums/src/main.rs
+++ 0009-enums/enums/src/main.rs
@@ -6,5 +6,12 @@ enum IpAddrKind {
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
+
+    route(four);
+    route(six);
+    route(IpAddrKind::V4);
 }
 
+fn route(ip_type:IpAddrKind) {
+    
+}
