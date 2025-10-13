b47f8ebcc97e0a81733bb7b0bf37ceec568b5d1c
Ali Aryani <lenesis@proton.me>
2025-10-13 17:55:40 +0330

    access and modify

--- 0008-structs/structs/src/main.rs
+++ 0008-structs/structs/src/main.rs
@@ -7,10 +7,16 @@ struct User {
 }
 
 fn main() {
-    let user1 = User {
+    let mut user1 = User {
         email: String::from("lenesis@proton.me"),
         username: String::from("aliaryani-dev"),
         active: true,
         sign_in_count: 1,
     };
+    
+    user1.username = String::from("aliaryani.dev");
+    println!("user1's email: {}",user1.email);
+    if user1.active {
+        println!("sign-in count: {}",user1.sign_in_count);
+    }
 }
