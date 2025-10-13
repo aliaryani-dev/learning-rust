e9d70af95c02d5fdc0c6bb23335d50f46889a0a5
Ali Aryani <lenesis@proton.me>
2025-10-13 10:49:59 +0330

    simple struct

--- 0008-structs/structs/src/main.rs
+++ 0008-structs/structs/src/main.rs
@@ -1,3 +1,11 @@
+// a simple struct demonstration for storing account info
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool
+}
+
 fn main() {
     println!("Hello World");
 }
