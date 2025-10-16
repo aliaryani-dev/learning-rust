a84f71fa21b71fde52e37ace88944956b7e814ad
Ali Aryani <lenesis@proton.me>
2025-10-16 11:16:01 +0330

    add loop labels

--- 0007-loops/loops/src/main.rs
+++ 0007-loops/loops/src/main.rs
@@ -3,6 +3,25 @@ fn main() {
     //loop {
     //    println!("again!");
     // }
+    // a while loop can have labels:
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining: {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+
     // while loop
     let mut number = 5;
     while number != 0 {
